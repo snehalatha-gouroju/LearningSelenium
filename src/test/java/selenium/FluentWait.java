38c6f65805e415df018e636347bb841daf2e16f9
snehalatha <snehagouroju804@gmail.com>
2026-09-04 19:16:47 +0530

    FluentWait

---
+++ src/test/java/selenium/FluentWait.java
@@ -0,0 +1,45 @@
+package selenium;
+
+import com.google.common.base.Function;
+import org.openqa.selenium.By;
+import org.openqa.selenium.ElementNotInteractableException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.NoSuchElementException;
+
+
+public class FluentWait {
+
+    public static void main(String[] args){
+        WebDriver driver=new ChromeDriver(); myWait=new FluentWait<WebDriver>(driver)
+                .withTimeout(Duration.ofSeconds(30))
+                .pollingEvery(Duration.ofSeconds(5))
+                .ignoring(NoSuchElementException.class);
+
+
+
+
+        driver.get("https://www.youtube.com");
+
+        driver.findElement(By.xpath("//input[@name='search_query']")).sendKeys("selenium");
+
+        WebElement Element=myWait.until(new Function<WebDriver,WebElement>(){
+            public WebElement apply(WebDriver driver){
+                return driver.findElement(
+                        driver.findElement(By.xpath("//span[text()=' tutorial for beginners']")).click();
+            }
+        });
+
+        Element.click();
+
+
+
+
+
+
+
+    }
+}
