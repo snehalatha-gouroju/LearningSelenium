2bcab146f54b040386aaf1cc30558233fd3811b6
snehalatha <snehagouroju804@gmail.com>
2026-09-12 12:45:25 +0530

    SumOfNumByUser_1

---
+++ src/test/java/JavaPrograms/SumOfNumByUser_1.java
@@ -0,0 +1,24 @@
+package JavaPrograms;
+
+import java.util.Scanner;
+
+public class SumOfNumByUser_1 {
+   public static void main(String[] args){
+       int num1,num2;
+
+       System.out.println("Enter 1st number ?");
+       Scanner sc=new Scanner(System.in);
+
+       num1= sc.nextInt();
+
+       System.out.println("Enter 1st number ?");
+
+
+       num2= sc.nextInt();
+
+       System.out.println("sum of the number is :"+(num1 + num2));
+
+   }
+
+
+}
