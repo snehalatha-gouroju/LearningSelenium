47b85bd9b882fc8e53bdb15895ddb642983025a7
snehalatha <snehagouroju804@gmail.com>
2026-09-14 10:22:05 +0530

    SumOfTwo_1

---
+++ src/test/java/JavaPractice/SumOfTwo_1.java
@@ -0,0 +1,24 @@
+package JavaPractice;
+
+
+import java.util.Scanner;
+
+public class SumOfTwo {
+
+    public static void main(String[] args){
+        //int sum=0;
+        System.out.println("Enter The FirstNum ?");
+
+         Scanner sc=new Scanner(System.in);
+
+         int firstNum=sc.nextInt();
+
+         System.out.println("Enter The SecondNum ?");
+
+         int SecondNum=sc.nextInt();
+
+        int sum=firstNum + SecondNum;
+        System.out.println("The sum of both numbers is :" +sum);
+
+}
+}
