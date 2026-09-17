f335c4c5e39e6082f2a2621cc21a2b721ee49a19
snehalatha <snehagouroju804@gmail.com>
2026-09-17 19:51:55 +0530

    FindSumOfNnum_5

---
+++ src/test/java/JavaPrograms/AvgOfArray_4.java
@@ -0,0 +1,38 @@
+package JavaPrograms;
+
+import java.lang.reflect.Array;
+
+public class AvgOfArray_4 {
+    //int numbers;
+
+    public static void main(String[] args){
+
+        int[] array = {2,3,4,5,6};
+
+        int length= array.length;
+
+
+
+        System.out.println("length of array is:" +length);
+
+        int sum=0;
+
+//       //* for (int j : array) {
+//
+//            sum = sum + j;
+//
+//        }
+
+        for(int i=0;i<length;i++) {
+            sum = sum + array[i];
+        }
+        System.out.println("the sum of array numbers :" +sum);
+
+
+        float avg=sum/length;
+        System.out.println("the avg of array numbers:" +avg);
+    }
+
+
+
+}
