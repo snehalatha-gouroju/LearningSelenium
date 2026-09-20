a60f7ea29313336bd1ae3a373b8d9f3e64f4fbef
snehalatha <snehagouroju804@gmail.com>
2026-09-20 23:59:12 +0530

    ReverseNumber.

---
+++ src/test/java/JavaPrograms/ReverseNumber.java
@@ -0,0 +1,32 @@
+package JavaPrograms;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+
+
+    public static void main(String[] args){
+
+        System.out.println("Enter any Number ?");
+
+        Scanner sc=new Scanner(System.in);
+
+        int number=sc.nextInt();
+
+        int res=0;
+
+        while( number > 0) {
+
+            int LastDigit = number % 10;
+
+
+            res = res * 10 +  LastDigit;
+
+            number = number / 10 ;
+        }
+
+        System.out.println("The Reverse Number Is : " +number);
+
+    }
+
+}
