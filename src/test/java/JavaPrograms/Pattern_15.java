5e9d71eff833925b3d69be3b7c551b220a269ce2
snehalatha <snehagouroju804@gmail.com>
2026-09-21 00:33:42 +0530

    Pattern_15

---
+++ src/test/java/JavaPrograms/Pattern_15.java
@@ -0,0 +1,27 @@
+package JavaPrograms;
+
+import java.util.Scanner;
+
+public class DuplicateCharString_14 {
+
+    public static void main(String[] args) {
+
+        int rows;
+
+        System.out.println("Enter NO.Of Rows ?");
+
+        Scanner sc = new Scanner(System.in);
+
+        rows = sc.nextInt();
+
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+
+            System.out.println();
+        }
+    }
+}
